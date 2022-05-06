dba27b6a37eb91b6369410da9a336e19521fac09
Delius Van Den Berg <deliusvdb14@gmail.com>
2022-05-06 11:24:04 +0200

    Published To Unity WebGL

---
+++ The Floor Is Lava/Library/Bee/artifacts/emscripten/rvz82/wrapper_emar.bat
@@ -0,0 +1,4 @@
+
+@echo off
+"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\2021.2.9f1\Editor\Data\PlaybackEngines\WebGLSupport\BuildTools\Emscripten\emscripten\emar.bat" %* < nul
+exit %ERRORLEVEL%
